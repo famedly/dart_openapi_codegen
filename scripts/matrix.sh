cf062e648b6b9792c4599b8bd18615748dbbbab5
Lukas Lihotzki <lukas@lihotzki.de>
2021-06-11 20:13:45 +0200

    feat: dartfmt in scripts/matrix.sh

--- scripts/matrix.sh
+++ scripts/matrix.sh
@@ -3,3 +3,4 @@ OUTPUT=${1?Output directory like matrix_api_lite/lib/src}
 ./scripts/prepare_matrix.sh
 cp output/*.dart "$1"/
 pub run dart_openapi_codegen "$1" matrix.json rules/matrix.yaml
+dartfmt -w "$1"/
