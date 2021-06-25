309ce82427f42b32f76faa21957af1c3b6dc0ffc
Lukas Lihotzki <lukas@lihotzki.de>
2021-06-25 12:47:02 +0200

    docs: improve README

--- scripts/matrix.sh
+++ scripts/matrix.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
-OUTPUT=${1?Output directory like matrix_api_lite/lib/src}
+OUTPUT=${1?Output directory like matrix_api_lite/lib/src/generated}
 ./scripts/prepare_matrix.sh
 cp output/*.dart "$1"/
 pub run dart_openapi_codegen "$1" matrix.json rules/matrix.yaml
