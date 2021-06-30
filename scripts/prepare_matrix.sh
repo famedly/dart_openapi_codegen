9af28eee6f283c742dd9a7ba6cd510f9a5b3a00a
Lukas Lihotzki <lukas@lihotzki.de>
2021-06-30 15:30:44 +0200

    fix: use unstable version for room_keys operations

--- scripts/prepare_matrix.sh
+++ scripts/prepare_matrix.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -e
 [ -d matrix-doc ] || git clone https://github.com/matrix-org/matrix-doc.git
-(
-	cd matrix-doc
-	./scripts/dump-swagger.py -c r0
-	sed -i 's`](/`](https://spec.matrix.org/unstable/`g' scripts/swagger/api-docs.json
-)
-ln -sf matrix-doc/scripts/swagger/api-docs.json matrix.json
+(cd matrix-doc && ./scripts/dump-swagger.py -c r0)
+rm matrix.json
+< matrix-doc/scripts/swagger/api-docs.json \
+sed 's`](/`](https://spec.matrix.org/unstable/`g' |
+jq '.paths |= with_entries(
+  if .key | contains("/room_keys/") then .key |= sub("/r0/";"/unstable/") else . end
+)' \
+> matrix.json
