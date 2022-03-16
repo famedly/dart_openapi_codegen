a660fd6651947ece7585caa1967ac1f771d6f826
Nicolas Werner <n.werner@famedly.com>
2022-03-16 15:51:17 +0100

    fix: macOS compat and version changes in scripts

--- scripts/matrix.sh
+++ scripts/matrix.sh
@@ -2,6 +2,7 @@
 OUTPUT=${1?Output directory like matrix_api_lite/lib/src/generated}
 ./scripts/prepare_matrix.sh
 cp output/*.dart "$1"/
-pub run dart_openapi_codegen "$1" matrix.json rules/matrix.yaml
-sed -i s/RoomKeysRequired/RoomKeys/g "$1"/api.dart
-dartfmt -w "$1"/
+dart run dart_openapi_codegen "$1" matrix.json rules/matrix.yaml
+# to make the sed command work on macos, use a backupfile
+sed -i'.bakmacoscompat' s/RoomKeysRequired/RoomKeys/g "$1"/api.dart
+dart format --fix "$1"/
