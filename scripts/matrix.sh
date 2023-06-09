26f49867241d40833e2c8e8a657110d95626fafd
Malin Errenst <m.errenst@famedly.com>
2023-06-09 15:16:09 +0200

    refactor: Removed unused line + added import sorting in scripts/matrix.sh

--- scripts/matrix.sh
+++ scripts/matrix.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -e
-OUTPUT=${1?Output directory like matrix_api_lite/lib/src/generated}
 ./scripts/prepare_matrix.sh
 cp output/*.dart "$1"/
 dart run dart_openapi_codegen "$1" matrix.json rules/matrix.yaml
 # to make the sed command work on macos, use a backupfile
 sed -i'.bakmacoscompat' s/RoomKeysRequired/RoomKeys/g "$1"/api.dart
-dart format --fix "$1"/
+cd "$1"
+dart format --fix .
+flutter pub run import_sorter:main --no-comments
