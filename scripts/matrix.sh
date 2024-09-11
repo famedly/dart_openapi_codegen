e95cc1afca9343ffc207e7ad4e702c61cb8ee013
td <j.nirve@famedly.com>
2024-09-11 16:24:28 +0200

    feat: add patches for v1.11 spec

--- scripts/matrix.sh
+++ scripts/matrix.sh
@@ -1,9 +1,15 @@
 #!/bin/sh -e
-./scripts/prepare_matrix.sh
+# ./scripts/prepare_matrix.sh
 cp output/*.dart "$1"/
 dart run dart_openapi_codegen "$1" matrix.json rules/matrix.yaml
 # to make the sed command work on macos, use a backupfile
 sed -i'.bakmacoscompat' s/RoomKeysRequired/RoomKeys/g "$1"/api.dart
 cd "$1"
-dart format --fix .
+dart pub get
+flutter pub run build_runner build --delete-conflicting-outputs
+dart fix --apply
+dart format -o write .
 flutter pub run import_sorter:main --no-comments
+# add any fixes after formatting
+dart fix --apply
+
